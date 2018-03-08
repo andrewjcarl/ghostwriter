70e472246d07f5ecb4a117ad9d97ee498a5e41fb
Andrew Carl <acarl@tesla.com>
2018-03-08 15:55:25 -0800

    add header component

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
+import Header from './components/header';
 
 class App extends Component {
   constructor(props){
@@ -19,10 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>

8aac0d6d56668cd1bfb2611f59184f9a1d0cd697
Bernard Nghiem <bnghiem@tesla.com>
2018-03-08 18:42:01 -0800

    added vote buttons to post. everything still ugly

--- app/src/components/Post/index.js
+++ app/src/components/Post/index.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Post extends Component {
+    handleUpvote = (post) => {
+    // Update votevalue through firebase call here
+    }
+
+    handleDownvote = (post) => {
+    // Update votevalue through firebase call here
+    }
+
     render() {
 
         if (this.props.loading) {
@@ -17,6 +25,21 @@ class Post extends Component {
                 </div>
                 <div>
                     Message: {this.props.post.message}
+                </div>
+                <div>
+                    <button
+                        onClick={ this.handleUpvote.bind(this, this.props.post) }
+                        type="button"
+                        >
+                        ^
+                    </button>
+                    <div>{this.props.post.votevalue}</div>
+                    <button
+                    onClick={ this.handleDownvote.bind(this, this.props.post) }
+                    type="button"
+                    >
+                    v
+                    </button>
                 </div>
             </div>
         );

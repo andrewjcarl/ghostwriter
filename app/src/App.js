56556c9ba8320bc3e1b246fb6781f50ef7cf910d
Tim Moos <tmoos1558@yahoo.com>
2018-03-08 17:35:40 -0800

    wire in firebase

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    var config = {
+      apiKey: "AIzaSyBAud0XGzC-z9gGwBj6PuKlyji0Tmq66hQ",
+      authDomain: "ghostwriterdb-ca2f1.firebaseapp.com",
+      databaseURL: "https://ghostwriterdb-ca2f1.firebaseio.com",
+      projectId: "ghostwriterdb-ca2f1",
+      storageBucket: "ghostwriterdb-ca2f1.appspot.com",
+      messagingSenderId: "471768156194"
+    };
+  firebase.initializeApp(config);
+  }
   render() {
     return (
       <div className="App">
